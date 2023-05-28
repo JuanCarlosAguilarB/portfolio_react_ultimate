24b40177507e1ec9ec6b6cfce07715304b2784b9
Juan Carlos A <abjuancarlos.12@gmail.com>
2023-05-28 04:07:28 -0500

    create the desing responsive of navbar

--- src/Components/Atoms/Navbar/Navbar.jsx
+++ src/Components/Atoms/Navbar/Navbar.jsx
@@ -1,24 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBarsStaggered } from "@fortawesome/free-solid-svg-icons";
+import { useMediaQuery } from "../../../Core/Hooks";
 
 const Navbar = () => {
+  const [widthWindowNavigator, heightWindowNavigator] = useMediaQuery();
+
+  const [isShowOptionsMenuHamburguer, setIsShowOptionsMenuHamburguer] =
+    useState(false);
+
+  const handleShowOptionsMenuHamburguer = () => {
+    setIsShowOptionsMenuHamburguer(!isShowOptionsMenuHamburguer);
+  };
+
   return (
     <>
       <nav className="navbar_container">
-        <h3>
+        <h3 style={{ width: "40px" }}>
           {" "}
           <span>JuanC.dev</span>{" "}
         </h3>
 
-        <ul className="navbar_container-list">
-          <Link className="navbar_container-list-link" to="/home">Home</Link>
-          <Link className="navbar_container-list-link" to="/About">About</Link>
-          <Link className="navbar_container-list-link" to="/Projects">Projects</Link>
-          <Link className="navbar_container-list-link" to="/Contact">Contact</Link>
-          <Link className="navbar_container-list-link" to="/CV">CV</Link>
+        <FontAwesomeIcon
+          icon={faBarsStaggered}
+          style={{
+            paddingRight: "12px",
+            display: widthWindowNavigator > 768 ? "none" : null,
+            width: "40px",
+          }}
+          onClick={handleShowOptionsMenuHamburguer}
+        />
+        <ul
+          className={
+            isShowOptionsMenuHamburguer
+              ? "display_none navbar_container-list"
+              : "navbar_container-list"
+          }
+          style={{ dysplay: "none" }}
+        >
+          <Link className="navbar_container-list-link" to="/home">
+            Home
+          </Link>
+          <Link className="navbar_container-list-link" to="/About">
+            About
+          </Link>
+          <Link className="navbar_container-list-link" to="/Projects">
+            Projects
+          </Link>
+          <Link className="navbar_container-list-link" to="/Contact">
+            Contact
+          </Link>
+          <Link className="navbar_container-list-link" to="/CV">
+            CV
+          </Link>
         </ul>
       </nav>
     </>
