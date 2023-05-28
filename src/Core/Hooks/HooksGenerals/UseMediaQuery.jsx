cdd49016d5a38b28034ef5502e4bd10785139b40
Juan Carlos A <abjuancarlos.12@gmail.com>
2023-05-28 04:06:59 -0500

    create custom hook for detect size of screen

---
+++ src/Core/Hooks/HooksGenerals/UseMediaQuery.jsx
@@ -0,0 +1,18 @@
+import React, { useLayoutEffect, useState } from 'react';
+
+
+export const useMediaQuery = () => {
+  const [screenSize, setScreenSize] = useState([0, 0]);
+
+  useLayoutEffect(() => {
+    function updateScreenSize() {
+      setScreenSize([window.innerWidth, window.innerHeight]);
+    }
+
+    window.addEventListener('resize', updateScreenSize);
+    updateScreenSize();
+    return () => window.removeEventListener('resize', updateScreenSize);
+  }, []);
+
+  return screenSize;
+};
