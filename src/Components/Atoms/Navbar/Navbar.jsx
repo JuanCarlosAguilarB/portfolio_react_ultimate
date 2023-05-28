69cb3085b91091f30afd90acf86f267e38333470
Juan Carlos A <abjuancarlos.12@gmail.com>
2023-05-28 03:10:19 -0500

    create navbar

--- src/Components/Atoms/Navbar/Navbar.jsx
+++ src/Components/Atoms/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-      <header>
-        <h1>My Super Cool App</h1>
-      </header>
+      <nav className="navbar_container">
+        <h3>
+          {" "}
+          <span>JuanC.dev</span>{" "}
+        </h3>
+
+        <ul className="navbar_container-list">
+          <Link className="navbar_container-list-link" to="/home">Home</Link>
+          <Link className="navbar_container-list-link" to="/About">About</Link>
+          <Link className="navbar_container-list-link" to="/Projects">Projects</Link>
+          <Link className="navbar_container-list-link" to="/Contact">Contact</Link>
+          <Link className="navbar_container-list-link" to="/CV">CV</Link>
+        </ul>
+      </nav>
     </>
   );
 };
